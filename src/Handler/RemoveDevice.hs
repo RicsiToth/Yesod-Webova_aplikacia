6c7a9a56f3d668bde1a35c46a9ae01d9fccb3f67
RicsiToth <50337780+RicsiToth@users.noreply.github.com>
2020-02-26 20:35:27 +0100

    Update RemoveDevice.hs

--- src/Handler/RemoveDevice.hs
+++ src/Handler/RemoveDevice.hs
@@ -7,10 +7,31 @@
 module Handler.RemoveDevice where
 
 import Import
+import Database.Persist.Sql
+import Data.Maybe
+import Data.Text as T
+import Text.Read as R
 
-getRemoveDeviceR :: Handler Html
-getRemoveDeviceR = error "Not yet implemented: getRemoveDeviceR"
+getRemoveDeviceR :: DeviceId -> Handler Html
+getRemoveDeviceR devId = do
+    sessId <- lookupSession "_ID"
+    let tmp = T.unpack $ fromJust sessId
+    let tmp2 = R.read tmp ::Int64
+    let uId = toSqlKey tmp2
+    valid <- validation uId devId
+    if(valid)
+        then do
+            _ <- runDB $ deleteWhere [ValuesDeviceId ==. devId]
+            _ <- runDB $ delete devId
+            redirect HomeR
+        else redirect HomeR --Možno dodatočnú správu
 
-postRemoveDeviceR :: Handler Html
-postRemoveDeviceR = error "Not yet implemented: postRemoveDeviceR"
+postRemoveDeviceR :: DeviceId -> Handler Html
+postRemoveDeviceR devId = getRemoveDeviceR devId
 
+validation :: UserId -> DeviceId -> Handler Bool
+validation uId devId = do
+  results <- runDB $ selectList [DeviceUserId ==. uId, DeviceId ==. devId] []
+  case results of
+    [] -> return False
+    _  -> return True
