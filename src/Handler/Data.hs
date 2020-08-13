763596363e1c619bf0d20606c5bb1a2aa3c38403
RicsiToth <50337780+RicsiToth@users.noreply.github.com>
2020-08-13 09:48:01 +0200

    Final

--- src/Handler/Data.hs
+++ src/Handler/Data.hs
@@ -12,8 +12,17 @@ module Handler.Data where
 
 import Import
 import Yesod.Core.Json (returnJson)
+import Database.Persist.Sql
 
-getDataR :: DeviceId -> Handler Value
-getDataR devId = do
+getDataR :: Text -> Handler Value
+getDataR uuid = do
+    devId <- getId uuid
     tmp <- runDB $ selectList [ValuesDeviceId ==. devId] [Asc ValuesTime]
     returnJson (map entityVal tmp)
+
+getId :: Text -> Handler DeviceId
+getId uuid = do
+  results <- runDB $ getBy $ UniqueUuid uuid
+  case results of
+    Nothing -> return (toSqlKey 0)
+    Just(Entity deviceId _) -> return deviceId
